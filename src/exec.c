265928505f260a4da83cf22aa97408d83eb50c5f
laidouni <mohamed.laidouni@etu.u-paris.fr>
2023-11-29 09:45:07 +0100

    cmd ? && cmd cd

--- src/exec.c
+++ src/exec.c
@@ -13,19 +13,18 @@ void exec_command(command_t *command)
     char *cmd = command->argv[0];
 
     if (strcmp(cmd, "?") == 0)
-    {
-        printf("%d\n", jsh.last_exit_code);
-    }
+        jsh.last_exit_code = showLastReturnCode();
     else if (strcmp(cmd, "pwd") == 0)
         jsh.last_exit_code = pwd();
     else if (strcmp(cmd, "cd") == 0)
         jsh.last_exit_code = cd(command->argv[1]);
     else if (strcmp(cmd, "exit") == 0)
         jsh.last_exit_code = quit(jsh.last_exit_code, command);
-    else if (fork() == 0) {
+    else if (fork() == 0)
+    {
         execvp(cmd, command->argv);
-		exit(1);
-	}
+        exit(1);
+    }
     else
     {
         int status;
