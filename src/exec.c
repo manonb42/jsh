1723259e4a8e6a72e549816fa7ebdf2acd87cca4
Manon Baha <baha.manon@gmail.com>
2023-12-02 18:09:17 +0100

    exec: add `exec_external` function

--- src/exec.c
+++ src/exec.c
@@ -8,24 +8,28 @@
 
 #include "internalcmd.h"
 
-void exec_command(command_t *command)
-{
-
+int exec_external(command_t *command){
     char *cmd = command->argv[0];
-    if (is_internal(cmd)) jsh.last_exit_code = exec_internal(command);
-    else if (fork() == 0)
-    {
+    if (fork() == 0) {
         execvp(cmd, command->argv);
         perror("jsh");
         free_command(command);
         jsh.last_exit_code = 127;
         exit(jsh.last_exit_code);
     }
-    else
-    {
+    else {
         int status;
         waitpid(0, &status, 0);
-        jsh.last_exit_code = WEXITSTATUS(status);
+        return WEXITSTATUS(status);
     }
 }
 
+void exec_command(command_t *command)
+{
+
+    char *cmd = command->argv[0];
+    int exit_code;
+    if (is_internal(cmd)) exit_code = exec_internal(command);
+    else exit_code = exec_external(command);
+    jsh.last_exit_code = exit_code;
+}
