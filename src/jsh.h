23ff33a1a2b8864d6b40dec82c6f71754ca69cbc
Manon Baha <baha.manon@gmail.com>
2023-12-10 20:26:23 +0100

    jsh: introduce `process_t` for running procs

--- src/jsh.h
+++ src/jsh.h
@@ -1,6 +1,7 @@
 #ifndef JSH_H_
 #define JSH_H_
 #include <stdbool.h>
+#include "vector.h"
 
 
 typedef struct command_t {
@@ -11,10 +12,18 @@ typedef struct command_t {
 } command_t;
 
 void free_command(command_t *command);
+
+typedef struct process_t {
+    int pid;
+} process_t;
+
+typedef struct vector process_v;
+
 typedef struct jsh_t
 {
     int last_exit_code;
     int pid_to_stop;
+    process_v processes;
 } jsh_t;
 
 extern jsh_t jsh;
