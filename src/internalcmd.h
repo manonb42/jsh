c876166fac5156f1b6684eac001fb8f312cccb13
Manon Baha <baha.manon@gmail.com>
2023-12-10 18:07:29 +0100

    internalcmd: header cleanups

--- src/internalcmd.h
+++ src/internalcmd.h
@@ -2,12 +2,7 @@
 #define INTERNALCMD_H_
 
 #include "jsh.h"
-#include "stdbool.h"
 
-int exec_pwd();
-int exec_cd(char *);
-int exec_exit(int, command_t *);
-int exec_show_last_return_code();
 bool is_internal(char *);
 int exec_internal(command_t *);
 void exec_external(command_t *command);
