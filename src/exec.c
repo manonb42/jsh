74ab2a94ba172572f80f65a9713780ea71a5769c
Manon Baha <baha.manon@gmail.com>
2023-12-10 23:44:42 +0100

    jsh: keep track of background processes

--- src/exec.c
+++ src/exec.c
@@ -13,8 +13,7 @@ void exec_command(command_t *command)
 
 	char *cmd = command->argv[0];
 
-	if (is_internal(cmd))
-	{
+	if (is_internal(cmd)) {
 		command->bg = false;
 		jsh.last_exit_code = exec_internal(command);
 	}
@@ -27,18 +26,20 @@ void exec_external(command_t *command)
 
 	int pid = fork();
 
-	if (pid == 0)
-	{
+	if (pid == 0) {
 		execvp(command->argv[0], command->argv);
 		perror("jsh");
 		free_command(command);
 		jsh.last_exit_code = 127;
 		exit(jsh.last_exit_code);
 	}
-	else if (!command->bg)
-	{
+	else if (!command->bg) {
 		int status;
 		waitpid(pid, &status, 0);
 		jsh.last_exit_code = WEXITSTATUS(status);
+	} else {
+		process_t *proc = calloc(sizeof(process_t), 1);
+		*proc = (process_t){ .pid = pid};
+		vector_append(&jsh.processes, proc);
 	}
 }
--- src/jsh.c
+++ src/jsh.c
@@ -8,6 +8,7 @@
 
 #include "input.h"
 #include "exec.h"
+#include <wait.h>
 
 jsh_t jsh = {0};
 
@@ -18,6 +19,22 @@ void free_command(command_t *command)
         free(command->argv[i]);
     free(command->argv);
     free(command);
+}
+
+
+void check_background_jobs(){
+    int pid;
+    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0){
+        for (int i=0; i<vector_length(&jsh.processes); ++i){
+            process_t* proc = vector_at(&jsh.processes, i);
+            if (proc->pid == pid){
+                vector_remove(&jsh.processes, i);
+                free(proc);
+                printf("%d\tDone\n", pid);
+                break;
+            }
+        }
+    }
 }
 
 int main()
@@ -31,6 +48,7 @@ int main()
         command_t *command;
         do {
             command = read_command();
+            check_background_jobs();
         } while( command == NULL );
 
         exec_command(command);
